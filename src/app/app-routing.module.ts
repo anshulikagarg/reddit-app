bb9640ffca19b99acbf018159a83bfb400960159
anshulikagarg <41734361+anshulikagarg@users.noreply.github.com>
2019-05-27 13:56:04 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { MyRedditComponent } from './my-reddit/my-reddit.component';
 import { RedditCommentComponent } from './reddit-comment/reddit-comment.component';
 
 const routes: Routes = [
-  {path: 'home/:id', component:RedditCommentComponent, pathMatch: 'full'},
+  {path: 'redditcomment/:id', component:RedditCommentComponent, pathMatch: 'full'},
   { path: '', component: MyRedditComponent, pathMatch: 'full'},
-  { path: 'home', component: RedditCommentComponent},
+  { path: 'home', component: MyRedditComponent, pathMatch: 'full'},
+  { path: 'text-align:center', component: RedditCommentComponent},
   { path: '**', redirectTo: '' }
 ];
 
